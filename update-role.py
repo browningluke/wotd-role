9ae83f8cdb4a959b3f33064adb62e895dc4d6e05
Luke Browning <browningluke01@gmail.com>
2021-12-11 16:40:35 -0800

    Add code

---
+++ update-role.py
@@ -0,0 +1,20 @@
+import requests, json, re, os, random
+
+GUILD_ID = os.getenv('GUILD_ID')
+ROLE_ID = os.getenv('ROLE_ID')
+# HCPING_ID = os.getenv('HCPING_ID')
+BOT_TOKEN = os.getenv('ADMIN_BOT_TOKEN')
+
+'''
+Get word of the day
+'''
+# resp = requests.get("https://www.merriam-webster.com/word-of-the-day")
+# wotd = re.search(">(.*)(?=<\/h1)", resp.text)[1]
+
+response = requests.patch(f"https://discord.com/api/v9/guilds/{GUILD_ID}/roles/{ROLE_ID}",
+    headers = {
+        'Authorization': f"Bot {BOT_TOKEN}",
+        'Content-Type': 'application/json'
+    }, data = json.dumps({ "name": random.randint(1, 1000) }))
+
+#requests.get(f"https://hc-ping.com/{HCPING_ID}") # Health check
