8c955caf69bd8983bdd86ef642ba71c8ac88dad0
Luke Browning <24992944+browningluke@users.noreply.github.com>
2022-02-05 20:56:34 -0500

    Add better logging

--- update-role.py
+++ update-role.py
@@ -11,10 +11,16 @@ Get word of the day
 resp = requests.get("https://www.merriam-webster.com/word-of-the-day")
 wotd = re.search(">(.*)(?=<\/h1)", resp.text)[1]
 
+print(f"Word of the day: {wotd}")
+
 response = requests.patch(f"https://discord.com/api/v9/guilds/{GUILD_ID}/roles/{ROLE_ID}",
     headers = {
         'Authorization': f"Bot {BOT_TOKEN}",
         'Content-Type': 'application/json'
     }, data = json.dumps({ "name": wotd }))
+
+if response.status_code != 200:
+    print(f"Status code: {response.status_code}\nBody: {response.text}")
+    raise "Non 200 status code"
 
 requests.get(f"https://hc-ping.com/{HCPING_ID}") # Health check
